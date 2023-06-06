eb44bfcc4c909f565a719791ff1588ce3c9bf327
Makerjunior <juninhobarbeiro142@gmail.com>
2023-06-06 19:29:39 -0300

    INNER JOIN

--- Banco/query1.sql
+++ Banco/query1.sql
@@ -1,5 +1,6 @@
 -- Active: 1685670931144@@127.0.0.1@3306@databas
 -- Criação de Banco de Dados
+#Criando tabela funcionarios
 CREATE Table funcionarios(
     id int UNSIGNED NOT NULL AUTO_INCREMENT,
     nome varchar(45) NOT NULL,
@@ -19,6 +20,7 @@ CREATE Table funcionarios(
     CONSTRAINT fk_veiculos_funcionarios FOREIGN KEY(funcionario_id) REFERENCES funcionarios(id)
   );
 
+ -- Criação da tabela salários
   CREATE TABLE salarios(
     faixa VARCHAR(45) NOT NULL,
     inicio DOUBLE  NOT NULL,
@@ -28,7 +30,6 @@ CREATE Table funcionarios(
 
 -- Alterando o nome do campo na tabela funcionarios
   ALTER TABLE funcionarios CHANGE COLUMN nome nome_funcionario VARCHAR(50) NOT NULL;
-  
   ALTER TABLE funcionarios CHANGE COLUMN nome_funcionario nome VARCHAR(50) NOT NULL;
 
   -- Deletando tabela
@@ -38,30 +39,60 @@ CREATE Table funcionarios(
   -- Criando índices para ordenação, manipulação, agrupamento e organização
   CREATE INDEX idex_departamentos ON funcionarios(departamento);
   
-  -- Oedena de acordocom os 6 primeiro caracteres
+  -- Ordena de acordo com os 6 primeiro caracteres
   CREATE INDEX index_nomes ON funcionarios(nome(6));
 
---Manipulação de dados
-SELECT * FROM funcionarios;
-INSERT INTO funcionarios(id,nome,salario,departamento) VALUES(1,"Junior",1200,"TI");
-INSERT INTO funcionarios(id,nome,salario,departamento) VALUES(2,"Gustavo",1300,"Infra estrutura");
-INSERT INTO funcionarios(nome,salario,departamento) VALUES("João",1400,"TI");
-INSERT INTO funcionarios(nome,salario,departamento) VALUES("Carlos",2000,"Infra Estrutura");
-INSERT INTO funcionarios(nome,salario,departamento) VALUES("José",1500,"Financeiro");
+-- Manipulação de dados
+ #Inserindo funcionarios 
+INSERT INTO funcionarios(nome,salario,departamento) VALUES('João',1400,'TI');
+INSERT INTO funcionarios(nome,salario,departamento) VALUES('Carlos',2000,'Infra Estrutura');
+INSERT INTO funcionarios(nome,salario,departamento) VALUES('José',1500,'Financeiro');
+-- podemos indicar um id desde que ele não exista 
+INSERT INTO funcionarios(id,nome,salario,departamento) VALUES(4,'Junior',1200,'TI');
+INSERT INTO funcionarios(id,nome,salario,departamento) VALUES(5,'Gustavo',1300,'Infra estrutura');
 
-INSERT INTO veiculos (funcionario_id, veiculo, placa) 
-VALUES (1, 'Carro', 'ABC1234');
 
-SELECT f.nome, v.veiculo, v.placa 
-FROM funcionarios f 
-JOIN veiculos v ON f.id = v.funcionario_id 
-WHERE f.departamento = 'TI';
-
-SELECT * FROM funcionarios;
 -- SELECT com filtro
-SELECT * FROM funcionarios WHERE departamento = "TI";
+SELECT * FROM funcionarios WHERE departamento = 'TI';
 SELECT * FROM funcionarios WHERE id=3;
+SELECT * FROM funcionarios WHERE  salario > 8000;
 
---Update de todas as colunas da tabela
+-- Update de todas as colunas da tabela
 SET SQL_SAFE_UPDATES=0;
-UPDATE funcionarios SET salario=salario *5;
+UPDATE funcionarios SET salario=ROUND(salario * 1.1, 2); # 10% de aumenton aredonta 2 casas decimais
+  
+--  AULA 
+DELETE FROM funcionarios WHERE  id=5;
+SELECT * FROM  funcionarios;
+
+
+-- Inserindo Veiculos
+INSERT INTO veiculos (funcionario_id, veiculo, placa) VALUES (1, 'Carro', 'ABC1234');
+INSERT INTO veiculos(funcionario_id, veiculo, placa) VALUES(1, 'Opala', 'DBA3465'); 
+INSERT INTO veiculos(funcionario_id, veiculo, placa) VALUES(4, 'Monza', 'ODH2376'); 
+
+SELECT * from veiculos;
+
+INSERT  INTO  salarios(faixa, inicio, fim ) VALUES('Analista Jr', 1000 ,2000);
+INSERT  INTO  salarios(faixa, inicio, fim ) VALUES('Analista Pleno', 2000 ,4000);
+
+-- Selec em apenas uma coluna
+SELECT nome AS 'Funcionario',
+salario AS 'Salario' FROM  funcionarios WHERE salario >1500;
+
+-- Unindo dois ou mais select
+SELECT * FROM funcionarios f  WHERE nome ='junior'
+UNION
+SELECT * FROM funcionarios f WHERE nome ='Carlos';
+
+--  Relacionamentos JOIN
+
+SELECT * FROM  funcionarios;
+SELECT  * FROM veiculos;
+
+SELECT  * FROM funcionarios INNER JOIN veiculos ON funcionario_id =funcionarios.id;
+SELECT  * FROM funcionarios f INNER JOIN veiculos v ON v.funcionario_id =f.id;
+
+
+
+
